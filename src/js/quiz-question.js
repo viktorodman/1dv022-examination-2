2367fbe821a7825e00d6048ebf7ba1e7ff475fc7
Viktor Ödman <odman94@gmail.com>
2019-11-27 11:30:30 +0100

    Added quiz-question.js

---
+++ src/js/quiz-question.js
@@ -0,0 +1,15 @@
+const template = document.createElement('template')
+template.innerHTML = `
+<p>Hello World</p>
+`
+
+class QuizQuestion extends window.HTMLElement {
+  constructor () {
+    super()
+
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+  }
+}
+
+window.customElements.define('quiz-question', QuizQuestion)

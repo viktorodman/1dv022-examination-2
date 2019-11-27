7d22367e382d24c7a1b66c67b9a482d0f7ddb900
Viktor Ödman <odman94@gmail.com>
2019-11-27 16:26:46 +0100

    Added createTextForm method

--- src/js/quiz-question.js
+++ src/js/quiz-question.js
@@ -1,7 +1,67 @@
 
 const template = document.createElement('template')
 template.innerHTML = `
-<p>Hello World</p>
+<style>
+.quizContainer {
+    width: 900px;
+    height: 700px;
+    background-color: #333;
+    margin: 0 auto;
+}
+.quizHeader {
+    /* width: 90%; */
+    height: 10%;
+    background-color: blue;
+    font-size: 50px;
+    margin: 0 auto;
+    text-align: center;
+}
+.quizMain {
+    /* width: 90%; */
+    height: 40%;
+    background-color: red;
+    margin: 0 auto;
+    text-align: center;
+}
+.quizInfo {
+    height: 10%;
+    background-color: yellow;
+    font-size: 30px;
+    padding: 10px;
+}
+.quizAnswer {
+    background-color: violet;
+    height: 40%;
+}
+.question {
+    margin: 0;
+    padding: 5px;
+    font-size: 40px;
+}
+.playerName {
+    float: right;
+}
+p {
+    padding: 0;
+    margin: 0;
+}
+</style>
+
+<div class="quizContainer">
+    <div class="quizHeader">
+        <p>Question: <span id="questionNumber"></span></p>
+    </div>
+    <div class="quizMain">
+        <p class="question"></p>
+    </div>
+    <div class="quizInfo">
+        <span class="timer">TIMER</span>
+        <span class="playerName">PLAYER: </span>
+    </div>
+    <div class="quizAnswer">
+        <p>hej</p>
+    </div>
+</div>
 `
 
 class QuizQuestion extends window.HTMLElement {
@@ -10,6 +70,33 @@ class QuizQuestion extends window.HTMLElement {
 
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(template.content.cloneNode(true))
+
+    this._currentQuestion = undefined
+    this.answerDiv = this.shadowRoot.querySelector('.quizAnswer')
+  }
+
+  async connectedCallback () {
+    await this.getQuestion()
+
+    this.createTextForm()
+    this.setQuestion()
+  }
+
+  async getQuestion () {
+    const pro = await window.fetch('http://vhost3.lnu.se:20080/question/1')
+    const res = await pro.json()
+
+    this._currentQuestion = res
+  }
+
+  setQuestion () {
+    const q = this.shadowRoot.querySelector('.question')
+
+    q.textContent = this._currentQuestion.question
+  }
+
+  createTextForm () {
+    this.answerDiv.innerHTML = ''
   }
 }
 
